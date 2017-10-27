9a7ee46e179377e9d278da69012d364916e74031
aegean <mailbox.aea@gmail.com>
2017-10-27 11:55:57 +0300

    create initial coordinate for graph

--- client/src/components/scheme/schemeComponent.js
+++ client/src/components/scheme/schemeComponent.js
@@ -14,7 +14,16 @@ function schemeComponentController(schemeEntity, FileUploader, API_ENDPOINT) {
   var vm = this;
   vm.$onInit = onInit;
   vm.createGraph = createGraph;
+  vm.getCoordinate = getCoordinate;
+  vm.getInitialCoordinate = getInitialCoordinate;
 
+  vm.model = new schemeEntity();
+  vm.model.start_coordinate = {};
+  vm.scheme = angular.element(document.querySelector("#scheme"));
+  vm.scheme.bind('click', getCoordinate);
+  vm.schemeArea = angular.element(document.querySelector("#scheme-area"));
+  vm.showBtnSetInitialCoordinate = false;
+  vm.isSetInitialCoordinate = false;
   vm.graph = [];
   vm.uploader = new FileUploader({
     url: API_ENDPOINT + 'imagelist/',
@@ -31,10 +40,7 @@ function schemeComponentController(schemeEntity, FileUploader, API_ENDPOINT) {
   }
   
   function createGraph(image, widthOfImage, heightOfImage) {
-      var scheme = angular.element(document.querySelector("#scheme"));
-      var schemeArea = angular.element(document.querySelector("#scheme-area"));
-
-      scheme.css('background-image',  'url(' + image + ')');
+      vm.scheme.css('background-image',  'url(' + image + ')');
       var width = Math.ceil(widthOfImage/10);
       var heigth = Math.ceil(heightOfImage/10);
 
@@ -57,8 +63,42 @@ function schemeComponentController(schemeEntity, FileUploader, API_ENDPOINT) {
              var td = angular.element('<td></td>');
              tr.append(td)
         }
-        schemeArea.append(tr);
+        vm.schemeArea.append(tr);
       }
+       vm.showBtnSetInitialCoordinate = true;
+  }
+
+  function getCoordinate(event) {
+    if( event.target.localName === 'td' && !vm.isSetInitialCoordinate) {
+            var cellIndex = event.target.cellIndex;
+            var parentTr = event.target.parentElement;
+            var rowIndex = parentTr.rowIndex;
+            event.target.style.backgroundColor = event.target.style.backgroundColor === ""? "red": "";
+            vm.graph[rowIndex][cellIndex] = vm.graph[rowIndex][cellIndex] === 1? 0: 1;
+    }
+  }
+  
+  function getInitialCoordinate() {
+      vm.isSetInitialCoordinate = true;
+      vm.scheme.bind('click', function (event) {
+          if( event.target.localName === 'td' && vm.isSetInitialCoordinate) {
+            var cellIndex = event.target.cellIndex;
+            var parentTr = event.target.parentElement;
+            var rowIndex = parentTr.rowIndex;
+
+            if(Object.keys(vm.model.start_coordinate).length) {
+                vm.model.start_coordinate = {};
+                var oldInitial = angular.element(document.querySelector(".initial"));
+                oldInitial[0].remove('initial');
+            }
+
+            vm.model.start_coordinate.latitude = cellIndex;
+            vm.model.start_coordinate.longitude = rowIndex;
+
+            event.target.classList.add("initial");
+            vm.isSetInitialCoordinate = false;
+          }
+      });
   }
 
 
