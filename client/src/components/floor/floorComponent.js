63edaf8ec0497ea9a07319df56dd4de0846fdd15
aegean <mailbox.aea@gmail.com>
2017-11-22 17:23:07 +0300

    fix get coordinate of object, draw and clear coordinate of object when coordinate is changed

--- client/src/components/floor/floorComponent.js
+++ client/src/components/floor/floorComponent.js
@@ -1,72 +1,73 @@
 angular
-  .module('app')
-  .component('floorComponent', {
-    templateUrl: '/src/components/floor/floorView.html',
-    bindings: {
-      buildings: '<'
-    },
-    controller: floorComponentController
-  })
+    .module('app')
+    .component('floorComponent', {
+      templateUrl: '/src/components/floor/floorView.html',
+      bindings: {
+        buildings: '<'
+      },
+      controller: floorComponentController
+    })
 ;
 
-function floorComponentController(floorEntity, FileUploader, API_ENDPOINT) {
+function floorComponentController(floorEntity, FileUploader, API_ENDPOINT, $scope) {
 
   var vm = this;
   vm.$onInit = onInit;
-  vm.createGraph = createGraph;
+  vm.createMap = createMap;
+  vm.getImage = getImage;
   vm.saveScheme = saveScheme;
   vm.addRoom = addRoom;
   vm.addTerminal = addTerminal;
 
   vm.uploader = new FileUploader({
-    url: API_ENDPOINT + 'imagelist/',
+    url: API_ENDPOINT + 'map/',
     alias: 'image',
     autoUpload: true,
     headers: {},
     onSuccessItem: function (file, response) {
-        vm.model.map.image = response.id;
-        vm.createGraph(response.image, response.widthOfImage, response.heightOfImage);
-        vm.showForm = true;
+      vm.model.map = response.id;
+      createMap(response.image);
     }
   });
 
   function onInit() {
     vm.model = new floorEntity();
-    vm.model.map = {};
     vm.model.entrance = {};
     vm.model.rooms = [];
     vm.scheme = angular.element(document.querySelector("#scheme"));
-    vm.schemeArea = angular.element(document.querySelector("#scheme-area"));
+    vm.canvas = document.createElement("canvas");
+    vm.canvas.id = "container";
     vm.showForm = false;
-    vm.model.graph = [];
   }
-  
-  function createGraph(image, widthOfImage, heightOfImage) {
-      vm.scheme.css('background-image',  'url(' + image + ')');
-      var width = Math.ceil(widthOfImage/10);
-      var heigth = Math.ceil(heightOfImage/10);
 
-      /*
-        fill graph
-       */
-      for (var i = 0; i < heigth; i++ ) {
-        vm.model.graph[i] = [];
-        for(var j = 0; j < width; j++) {
-            vm.model.graph[i][j] = 1;
-        }
-      }
+  function createMap(url) {
+    getImage(url).then(function (img) {
+      var width = img.width;
+      var height = img.height;
+      vm.canvas.width = width % 2 === 0 ? width - 1 : width;
+      vm.canvas.height = height;
+      vm.scheme.append(vm.canvas);
+      var ctx = vm.canvas.getContext("2d");
+      ctx.drawImage(img, 0, 0);
+      vm.showForm = true;
+      $scope.$apply();
+    }).catch(function (img) {
+      console.log(img);
+    });
+  }
 
-      /*
-        create graph table
-       */
-      for (var h = 0; h < heigth; h++ ) {
-        var tr = angular.element('<tr></tr>');
-        for(var w = 0; w < width; w++) {
-             var td = angular.element('<td></td>');
-             tr.append(td)
-        }
-        vm.schemeArea.append(tr);
-      }
+  function getImage(url) {
+    return new Promise(function (resolve, reject) {
+      var img = new Image();
+      img.onload = function () {
+        resolve(img)
+      };
+      img.onerror = function () {
+        reject(img)
+      };
+      img.src = url;
+      img.setAttribute('crossOrigin', '');
+    })
   }
 
   function addRoom() {
@@ -80,7 +81,7 @@ function floorComponentController(floorEntity, FileUploader, API_ENDPOINT) {
   }
 
   function saveScheme() {
-    if(!Object.keys(vm.model.terminal.coordinate).length) {
+    if (!Object.keys(vm.model.terminal.coordinate).length) {
       delete vm.model.terminal;
     }
     console.log(vm.model);
--- client/src/components/selectCoordinate/secelectCoordinateComponent.js
+++ client/src/components/selectCoordinate/secelectCoordinateComponent.js
@@ -18,10 +18,16 @@ function selectCoordinateComponentController() {
   vm.getCoordinate = getCoordinate;
 
   function onInit() {
+    vm.terminal = {};
+    vm.entrance = {};
+    vm.building = {};
+    vm.room = {};
+    vm.rectWidht = 6;
+    vm.rectHeight = 6;
     vm.canvas = document.querySelector("#container");
     vm.angCanvas = angular.element(document.querySelector("#container"));
     vm.ctx = vm.canvas.getContext("2d");
-    vm.tmpImgData = vm.ctx.getImageData(0, 0, vm.canvas.width, vm.canvas.height);
+    // vm.tmpImgData = vm.ctx.getImageData(0, 0, vm.canvas.width, vm.canvas.height);
   }
 
   function activateAction() {
@@ -34,29 +40,55 @@ function selectCoordinateComponentController() {
     var x = event.clientX - rect.left;
     var y = event.clientY - rect.top;
 
-    vm.ctx.clearRect(0, 0, vm.canvas.width, vm.canvas.height);
-    vm.ctx.putImageData(vm.tmpImgData, 0, 0);
+    // vm.ctx.clearRect(0, 0, vm.canvas.width, vm.canvas.height);
+    // vm.ctx.putImageData(vm.tmpImgData, 0, 0);
+
+    vm.coordinate.longitude = Math.round(x);
+    vm.coordinate.latitude = Math.round(y);
 
     switch (vm.name) {
       case 'terminal':
+        if (Object.keys(vm.terminal).length) {
+          vm.ctx.clearRect(vm.terminal.longitude, vm.terminal.latitude, vm.rectWidht, vm.rectHeight);
+        }
+        vm.terminal.longitude = vm.coordinate.longitude;
+        vm.terminal.latitude = vm.coordinate.latitude;
+
         vm.ctx.fillStyle = "#336600";
-        vm.ctx.fillRect(x, y, 6, 6);
+        vm.ctx.fillRect(x, y, vm.rectWidht, vm.rectHeight);
         break;
       case 'building':
+        if (Object.keys(vm.building).length) {
+          vm.ctx.clearRect(vm.building.longitude, vm.building.latitude, vm.rectWidht, vm.rectHeight);
+        }
+        vm.building.longitude = vm.coordinate.longitude;
+        vm.building.latitude = vm.coordinate.latitude;
+
         vm.ctx.fillStyle = "#00FFFF";
-        vm.ctx.fillRect(x, y, 6, 6);
+        vm.ctx.fillRect(x, y, vm.rectWidht, vm.rectHeight);
         break;
       case 'room':
+        if (Object.keys(vm.room).length) {
+          vm.ctx.clearRect(vm.room.longitude, vm.room.latitude, vm.rectWidht, vm.rectHeight);
+        }
+        vm.room.longitude = vm.coordinate.longitude;
+        vm.room.latitude = vm.coordinate.latitude;
+
         vm.ctx.fillStyle = "#FFFF00";
-        vm.ctx.fillRect(x, y, 6, 6);
+        vm.ctx.fillRect(x, y, vm.rectWidht, vm.rectHeight);
         break;
       case 'entrance':
+        if (Object.keys(vm.entrance).length) {
+          vm.ctx.clearRect(vm.entrance.longitude, vm.entrance.latitude, 6, 6);
+        }
+        vm.entrance.longitude = vm.coordinate.longitude;
+        vm.entrance.latitude = vm.coordinate.latitude;
+
         vm.ctx.fillStyle = "#606060";
-        vm.ctx.fillRect(x, y, 6, 6);
+        vm.ctx.fillRect(x, y, vm.rectWidht, vm.rectHeight);
         break;
     }
-    vm.coordinate.longitude = Math.round(x);
-    vm.coordinate.latitude = Math.round(y);
+
     console.log(vm.coordinate);
   }
 
