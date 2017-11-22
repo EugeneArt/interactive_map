a94b3d4ef952b246d29b27e127a6a8c1282db18c
aegean <mailbox.aea@gmail.com>
2017-11-22 19:57:07 +0300

    create map with route by query

--- client/src/components/route/routeComponent.js
+++ client/src/components/route/routeComponent.js
@@ -1,24 +1,28 @@
 angular
-  .module('app')
-  .component('routeComponent', {
-    templateUrl: '/src/components/route/routeView.html',
-    bindings: { },
-    controller: routeComponentController
-  })
+    .module('app')
+    .component('routeComponent', {
+      templateUrl: '/src/components/route/routeView.html',
+      bindings: {},
+      controller: routeComponentController
+    })
 ;
 
-function routeComponentController(findPathEntity, imageEntity,  FLOOR_ID) {
+function routeComponentController(findPathEntity, mapEntity, FLOOR_ID) {
 
   var vm = this;
   vm.$onInit = onInit;
   vm.getPath = getPath;
-  vm.createGraph = createGraph;
+  vm.createMap = createMap;
+  vm.getImage = getImage;
+  vm.createBinaryArray = createBinaryArray;
+  vm.drawPath = drawPath;
 
   function onInit() {
-    vm.scheme = angular.element(document.querySelector("#scheme"));
-    vm.schemeArea = angular.element(document.querySelector("#scheme-area"));
+    vm.scheme = angular.element(document.querySelector("#currentFloor"));
+    vm.canvas = document.createElement("canvas");
+    vm.canvas.id = "container";
   }
-  
+
   function getPath() {
 
     var params = {
@@ -31,56 +35,93 @@ function routeComponentController(findPathEntity, imageEntity,  FLOOR_ID) {
     findPathEntity.fetchAll(params).then(success, fail);
 
     function success(result) {
-      // var path = result[0];
-      // var terminal = result[0].current_floor.terminal.coordinate;
-      // var endpoint = path.coordinate;
-      // var img_id = result[0].current_floor.map.image;
-      //
-      // var graph = new Graph(path.current_floor.graph);
-      //
-      // var start = graph.grid[terminal.longitude][terminal.latitude];
-      // var end = graph.grid[endpoint.longitude][endpoint.latitude];
-      //
-      // var route = astar.search(graph, start, end);
-      // console.log(route);
-      //
-      // imageEntity.fetchOne(img_id).then(function (result) {
-      //   vm.createGraph(result.image, result.widthOfImage, result.heightOfImage, route);
-      // });
-      console.log(result);
+      var data = result[0];
+      var currentFloor = data.currentFloor;
+      var roomCoordinate = data.roomCoordinate;
+
+      mapEntity.fetchOne(currentFloor.map).then(function (response) {
+        createMap(response.image, currentFloor.terminal.coordinate, roomCoordinate);
+      });
     }
 
     function fail(error) {
       console.log(error);
     }
-
-
   }
 
-  function createGraph(image, widthOfImage, heightOfImage, route) {
-      vm.scheme.css('background-image',  'url(' + image + ')');
-      var width = Math.ceil(widthOfImage/10);
-      var heigth = Math.ceil(heightOfImage/10);
+  function createMap(url, start, end) {
+    getImage(url).then(function (img) {
+      var width = img.width;
+      var height = img.height;
+      vm.canvas.width = width % 2 === 0 ? width - 1 : width;
+      vm.canvas.height = height;
+      vm.scheme.append(vm.canvas);
+      var ctx = vm.canvas.getContext("2d");
+      ctx.drawImage(img, 0, 0);
 
-      /*
-        create graph table
-       */
-      for (var h = 0; h < heigth; h++ ) {
-        var tr = angular.element('<tr></tr>');
-        for(var w = 0; w < width; w++) {
-             var td = angular.element('<td id="map_'+ h + w + '"></td>');
-             tr.append(td)
-        }
-        vm.schemeArea.append(tr);
+      var arr = createBinaryArray();
+      drawPath(arr, start, end);
+    }).catch(function (img) {
+      console.log(img);
+    });
+  }
+
+  function getImage(url) {
+    return new Promise(function (resolve, reject) {
+      var img = new Image();
+      img.onload = function () {
+        resolve(img)
+      };
+      img.onerror = function () {
+        reject(img)
+      };
+      img.src = url;
+      img.setAttribute('crossOrigin', '');
+    })
+  }
+
+  function createBinaryArray() {
+    var ctx = vm.canvas.getContext("2d");
+    var map = ctx.getImageData(0, 0, vm.canvas.width, vm.canvas.height);
+    var imdata = map.data;
+    var r, g, b;
+    var currentInnerArray;
+    var zeroesAndOnes = [];
+    for (var p = 0, len = imdata.length; p < len; p += 4) {
+      r = imdata[p];
+      g = imdata[p + 1];
+      b = imdata[p + 2];
+
+      if (p % vm.canvas.width * 4 === 0) {
+        currentInnerArray = [];
+        zeroesAndOnes.push(currentInnerArray);
       }
-       route.forEach(function (item) {
-            var cell = document.getElementById('map_' + item.x + item.y);
-            cell.style.backgroundColor = "red";
-        });
-
+      if (r === 255 && g === 255 && b === 255) {
+        currentInnerArray.push(1);
+      } else {
+        currentInnerArray.push(0);
+      }
+    }
+    ctx.putImageData(map, 0, 0);
+    return zeroesAndOnes;
   }
-  
-  
 
+  function drawPath(arr, start, end) {
+    var graph = new Graph(arr, {diagonal: true});
+    var startCoordinate = graph.grid[start.longitude][start.latitude];
+    var endCoordinate = graph.grid[end.longitude][end.latitude];
+
+    var path = astar.search(graph, startCoordinate, endCoordinate);
+    console.log(graph, path);
+
+    var ctx = vm.canvas.getContext("2d");
+    ctx.fillStyle = "#00ff00";
+    ctx.fillRect(start.latitude, start.longitude, 8, 8);
+    ctx.fillRect(end.latitude, end.longitude, 8, 8);
+
+    for (var i = 0; i < path.length; i += 9) {
+      ctx.fillRect(path[i].y, path[i].x, 4, 4);
+    }
+  }
 
 }
