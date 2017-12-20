f6b2909549657505d39e76c63753f5dcbc7d1c8b
eugeneart <artsiukhevicheugene@gmail.com>
2017-12-20 11:12:09 +0300

    fix functionality of drawing images

--- client/src/components/selectCoordinate/secelectCoordinateComponent.js
+++ client/src/components/selectCoordinate/secelectCoordinateComponent.js
@@ -1,13 +1,13 @@
 angular
-    .module('app')
-    .component('selectCoordinateComponent', {
-      templateUrl: '/src/components/selectCoordinate/selectCoordinateView.html',
-      bindings: {
-        coordinate: '<',
-        name: '@'
-      },
-      controller: selectCoordinateComponentController
-    });
+  .module('app')
+  .component('selectCoordinateComponent', {
+    templateUrl: '/src/components/selectCoordinate/selectCoordinateView.html',
+    bindings: {
+      coordinate: '<',
+      name: '@'
+    },
+    controller: selectCoordinateComponentController
+  });
 
 function selectCoordinateComponentController() {
 
@@ -15,42 +15,16 @@ function selectCoordinateComponentController() {
   vm.$onInit = onInit;
   vm.activateAction = activateAction;
   vm.getCoordinate = getCoordinate;
+  vm.draw = draw;
+  vm.drawPoints = drawPoints;
 
   function onInit() {
-    console.log(vm.coordinate, vm.name);
-    vm.rectWidht = 6;
-    vm.rectHeight = 6;
+    vm.rectSize = 6;
     vm.canvas = document.querySelector("#container").childNodes[0];
     vm.angCanvas = angular.element(document.querySelector("#container"));
     vm.ctx = vm.canvas.getContext("2d");
 
-    switch (vm.name) {
-      case 'terminal':
-        vm.terminal = vm.coordinate;
-        vm.ctx.fillStyle = "#336600";
-        vm.ctx.fillRect(vm.coordinate.x, vm.coordinate.y, vm.rectWidht, vm.rectHeight);
-        break;
-      case 'entrance':
-        vm.entrance = vm.coordinate;
-        vm.ctx.fillStyle = "#606060";
-        vm.ctx.fillRect(vm.coordinate.x, vm.coordinate.y, vm.rectWidht, vm.rectHeight);
-        break;
-      case 'room':
-        vm.room = vm.coordinate;
-        vm.ctx.fillStyle = "#FFFF00";
-        vm.ctx.fillRect(vm.coordinate.x, vm.coordinate.y, vm.rectWidht, vm.rectHeight);
-        break;
-      case 'building':
-        vm.building = vm.coordinate;
-        vm.ctx.fillStyle = "#00FFFF";
-        vm.ctx.fillRect(vm.coordinate.x, vm.coordinate.y, vm.rectWidht, vm.rectHeight);
-        break;
-      case 'passageway':
-        vm.passageway = vm.coordinate;
-        vm.ctx.fillStyle = "#791fd4";
-        vm.ctx.fillRect(vm.coordinate.x, vm.coordinate.y, vm.rectWidht, vm.rectHeight);
-        break;
-    }
+    vm.drawPoints();
   }
 
   function activateAction() {
@@ -59,7 +33,7 @@ function selectCoordinateComponentController() {
   }
 
   function getCoordinate(event) {
-    vm.ctx.clearRect(vm.coordinate.x, vm.coordinate.y, vm.rectWidht, vm.rectHeight);
+    vm.ctx.clearRect(vm.coordinate.x, vm.coordinate.y, vm.rectSize, vm.rectSize);
 
     var rect = vm.canvas.getBoundingClientRect();
     var x = event.clientX - rect.left;
@@ -68,44 +42,37 @@ function selectCoordinateComponentController() {
     vm.coordinate.x = Math.round(x);
     vm.coordinate.y = Math.round(y);
 
+    vm.drawPoints();
+  }
+
+  function drawPoints() {
     switch (vm.name) {
       case 'terminal':
-        vm.terminal.x = vm.coordinate.x;
-        vm.terminal.y = vm.coordinate.y;
-
-        vm.ctx.fillStyle = "#336600";
-        vm.ctx.fillRect(x, y, vm.rectWidht, vm.rectHeight);
-        break;
-      case 'building':
-        vm.building.x = vm.coordinate.x;
-        vm.building.y = vm.coordinate.y;
-
-        vm.ctx.fillStyle = "#00FFFF";
-        vm.ctx.fillRect(x, y, vm.rectWidht, vm.rectHeight);
-        break;
-      case 'room':
-        vm.room.x = vm.coordinate.x;
-        vm.room.y = vm.coordinate.y;
-
-        vm.ctx.fillStyle = "#FFFF00";
-        vm.ctx.fillRect(x, y, vm.rectWidht, vm.rectHeight);
+        vm.terminal = vm.coordinate;
+        vm.draw("#336600");
         break;
       case 'entrance':
-        vm.entrance.x = vm.coordinate.x;
-        vm.entrance.y = vm.coordinate.y;
-
-        vm.ctx.fillStyle = "#606060";
-        vm.ctx.fillRect(x, y, vm.rectWidht, vm.rectHeight);
+        vm.entrance = vm.coordinate;
+        vm.draw("#606060");
+        break;
+      case 'room':
+        vm.room = vm.coordinate;
+        vm.draw("#FFFF00");
+        break;
+      case 'building':
+        vm.building = vm.coordinate;
+        vm.draw("#00FFFF");
         break;
       case 'passageway':
-        vm.passageway.x = vm.coordinate.x;
-        vm.passageway.y = vm.coordinate.y;
-
-        vm.ctx.fillStyle = "#791fd4";
-        vm.ctx.fillRect(x, y, vm.rectWidht, vm.rectHeight);
+        vm.passageway = vm.coordinate;
+        vm.draw("#791fd4");
         break;
     }
+  }
 
+  function draw(color) {
+    vm.ctx.fillStyle = color;
+    vm.ctx.fillRect(vm.coordinate.x, vm.coordinate.y, vm.rectSize, vm.rectSize);
   }
 
 }
