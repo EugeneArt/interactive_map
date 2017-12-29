57ed6fd12aef7617fdab27d1eb3cb472b90ab499
eugeneart <artsiukhevicheugene@gmail.com>
2017-12-29 16:31:56 +0300

    fix room form styles

--- client/src/components/roomForm/roomFormComponent.js
+++ client/src/components/roomForm/roomFormComponent.js
@@ -12,10 +12,14 @@ function roomFormComponentController() {
 
   var vm = this;
   vm.$onInit = onInit;
-
+  vm.removeRoom = removeRoom;
 
   function onInit() {
     vm.room.coordinate = vm.room.coordinate || {};
+  }
+
+  function removeRoom() {
+    delete vm.room;
   }
 
 }
